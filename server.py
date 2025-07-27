4c21d306bd76362c80c9acaf003cd6bdbd0acf9e
BlackBeard348 <cmorgan348@outlook.com>
2025-07-27 00:23:23 -0400

    Tasks Complete

--- server.py
+++ server.py
@@ -7,40 +7,29 @@ from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 app = Flask("Sentiment Analyzer")
 
-
+""" This is the GET route for retrieving the data and providing the response"""
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    # Retrieve the text to analyze from the request arguments
+    """Retrieve the text to analyze from the request arguments"""
     text_to_analyze = request.args.get('textToAnalyze')
-
-    # Pass the text to the sentiment_analyzer function and store the response
+    #ass the text to the sentiment_analyzer function and store the response
     response = sentiment_analyzer(text_to_analyze)
-
-    # Extract the label and score from the response
+    #Extract the label and score from the response
     label = response['label']
     score = response['score']
-
-"""error handling using else-if"""
-# Check if the label is None, indicating an error or invalid input
+    #error handling
+    #Check if the label is None, indicating an error or invalid input
     if label is None:
         return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-    
+    #Return a formatted string with the sentiment label and score
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
+#This function initiates the rendering of the main applicationpage over the Flask channel
 @app.route("/")
 def render_index_page():
+    """returns the index tenplate information"""
     return render_template('index.html')
-    
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
 
-
+#This functions executes the flask app and deploys it on localhost:5000
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
-#####
+    app.run(host="0.0.0.0", port=5002)
